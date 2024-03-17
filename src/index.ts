68295e7d92ae99f02e9dbe78a3f9d51aedc44da7
habib33-3 <habib.swi16@gmail.com>
2024-03-18 01:04:53 +0600

    express with ts initiated

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({ message: "server running" });
+});
+
+app.listen(port, () => {
+  console.log(`server started at ${port}`);
+});
