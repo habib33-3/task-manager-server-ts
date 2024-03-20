a1d5868aac7cd7244db7077a720218afd77cc8f6
habib33-3 <habib.swi16@gmail.com>
2024-03-20 14:59:09 +0600

    get tasks

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -21,4 +21,22 @@ const addTask = async (req: Request, res: Response) => {
     }
 };
 
-export { addTask };
+const getTasks = async (req: Request, res: Response) => {
+    try {
+        const tasks = Task.find();
+
+        res.status(200).json({
+            message: "Task loaded",
+            success: true,
+            tasks,
+        });
+    } catch (error) {
+        console.log("error during get task", error);
+        res.status(500).json({
+            message: "error during get task",
+            success: false,
+        });
+    }
+};
+
+export { addTask, getTasks };
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,8 +1,10 @@
-import { addTask } from "@/controllers/task.controller";
+import { addTask, getTasks } from "@/controllers/task.controller";
 import { Router } from "express";
 
 const taskRouter: Router = Router();
 
 taskRouter.post("/tasks", addTask);
+
+taskRouter.get("/tasks", getTasks);
 
 export default taskRouter;
