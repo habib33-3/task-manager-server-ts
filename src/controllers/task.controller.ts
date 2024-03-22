14587f0c005a3775f565d1bcce59cc0eebb223dd
habib33-3 <habib.swi16@gmail.com>
2024-03-22 21:01:38 +0600

    delete

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,5 +1,6 @@
 import Task from "@/models/task.model";
 import { Response, Request } from "express";
+import { ObjectId } from "mongoose";
 
 const addTask = async (req: Request, res: Response) => {
     try {
@@ -38,5 +39,28 @@ const getTasks = async (req: Request, res: Response) => {
         });
     }
 };
+
+
+const deleteTask=async (req:Request,res:Response)=>{
+try {
+    const {id}=req.params
+
+    const result=Task.findByIdAndDelete(id)
+
+    if(result!==null){
+        res.status(200).json({
+            message:"Task deleted",
+            success:true
+        })
+    }
+} catch (error) {
+    console.log("error during delete task", error);
+        res.status(500).json({
+            message: "error during delete task",
+            success: false,
+        });
+    }
+}
+}
 
 export { addTask, getTasks };
